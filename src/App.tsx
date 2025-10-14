500b5a3d73a9553cc00ce13e3e3035e81d598d8d
Rahul P <rahul.cd23@bitsathy.ac.in>
2025-10-14 17:22:36 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import { useState } from "react";
 import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
 import SpeedTestCard from "./components/SpeedTestCard";
 import SpeedFloat from "./components/SpeedFloat";
 
 export default function App() {
-  const [page, setPage] = useState<"home"|"about"|"login"|"float">("home");
+  const [page, setPage] = useState<"home" | "about" | "login" | "float">("home");
 
   return (
     <div className="flex min-h-screen">
       <Sidebar page={page} setPage={setPage} />
 
       <div className="flex-1 ml-20 p-10">
-        {page === "home" && <h1 className="text-5xl font-bold">Home Page</h1>}
-        {page === "about" && <h1 className="text-5xl font-bold">About Page</h1>}
-        {page === "login" && <h1 className="text-5xl font-bold">Login Page</h1>}
+        {page === "home" && <Home />}
+        {page === "about" && <About />}
+        {page === "login" && <Login />}
         {page === "float" && <SpeedTestCard />}
       </div>
 
