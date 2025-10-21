6421f5d1003d6db72c638b2f7fc6dfdff4ad9435
Rahul P <rahul.cd23@bitsathy.ac.in>
2025-10-21 21:51:50 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -20,10 +20,10 @@ app.add_middleware(
 
 def get_db():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
+        host=os.getenv("localhost"),
+        user=os.getenv("root"),
+        password=os.getenv("rahul18"),
+        database=os.getenv("authdb"),
     )
 
 @app.post("/signup")
